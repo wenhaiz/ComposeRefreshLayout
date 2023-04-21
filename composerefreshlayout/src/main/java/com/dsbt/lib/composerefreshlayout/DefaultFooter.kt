77e8c38c9d16049f1cb28d4950fc2db292775938
wenhaiz <wenhaiz.me@gmail.com>
2023-04-21 09:54:23 +0800

    Refine header and footer,add string resources

--- composerefreshlayout/src/main/java/com/dsbt/lib/composerefreshlayout/DefaultFooter.kt
+++ composerefreshlayout/src/main/java/com/dsbt/lib/composerefreshlayout/DefaultFooter.kt
@@ -16,6 +16,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 
 
@@ -27,12 +28,14 @@ fun DefaultRefreshFooter(state: RefreshLayoutState) {
     }
     val newText = when {
         state.isLoadingMore.state == State.Resetting -> ""
-        (state.isLoadingMore.state == State.IDLE || state.isLoadingMore.state == State.Dragging) && !state.isLoadingMore.hasMoreData -> "没有更多数据了"
-        state.isLoadingMore.state == State.InProgress -> "正在加载..."
-        state.isLoadingMore.state == State.Success -> "加载成功"
-        state.isLoadingMore.state == State.Failed -> "加载失败"
-        state.isLoadingMore.state == State.ReadyForAction -> "松开加载更多"
-        else -> "上滑加载更多"
+        (state.isLoadingMore.state == State.IDLE || state.isLoadingMore.state == State.Dragging) && !state.isLoadingMore.hasMoreData -> stringResource(
+            id = R.string.footer_no_more
+        )
+        state.isLoadingMore.state == State.InProgress -> stringResource(id = R.string.footer_refreshing)
+        state.isLoadingMore.state == State.Success -> stringResource(id = R.string.footer_complete)
+        state.isLoadingMore.state == State.Failed -> stringResource(id = R.string.footer_failed)
+        state.isLoadingMore.state == State.ReadyForAction -> stringResource(id = R.string.footer_pulling)
+        else -> stringResource(id = R.string.footer_idle)
     }
     if (newText.isNotEmpty()) {
         text = newText
--- composerefreshlayout/src/main/java/com/dsbt/lib/composerefreshlayout/DefaultHeader.kt
+++ composerefreshlayout/src/main/java/com/dsbt/lib/composerefreshlayout/DefaultHeader.kt
@@ -1,8 +1,15 @@
 package com.dsbt.lib.composerefreshlayout
 
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -11,32 +18,73 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
+
+private val State.isFinished: Boolean
+    get() = this == State.Success || this == State.Failed || this == State.Resetting
 
 
 @Composable
 fun DefaultRefreshHeader(state: RefreshLayoutState) {
-    val heightDp = 80.dp
-    var text by remember {
-        mutableStateOf("")
-    }
-    val newText = when {
-        state.isRefreshing.state == State.Resetting -> ""
-        (state.isRefreshing.state == State.IDLE || state.isRefreshing.state == State.Dragging) && !state.isRefreshing.hasMoreData -> "没有更多数据了"
-        state.isRefreshing.state == State.InProgress -> "正在刷新"
-        state.isRefreshing.state == State.Success -> "刷新成功"
-        state.isRefreshing.state == State.Failed -> "刷新失败"
-        state.isRefreshing.state == State.ReadyForAction -> "松开刷新"
-        else -> "下拉可以刷新"
-    }
-    if (newText.isNotEmpty()) {
-        text = newText
-    }
     Box(
         modifier = Modifier
-            .height(heightDp)
+            .height(80.dp)
             .fillMaxWidth()
     ) {
-        Text(text = text, modifier = Modifier.align(Alignment.Center))
+        val agree =
+            if (state.isRefreshing.state == State.ReadyForAction || state.isRefreshing.state == State.InProgress) {
+                90f
+            } else {
+                -90f
+            }
+        val rotation by animateFloatAsState(targetValue = agree)
+        Row(
+            modifier = Modifier
+                .align(Alignment.Center)
+                .fillMaxHeight(),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            if (state.isRefreshing.state == State.InProgress) {
+                CircularProgressIndicator(
+                    modifier = Modifier
+                        .padding(end = 12.dp)
+                        .size(24.dp),
+                    color = Color(0xFF808080),
+                    strokeWidth = 2.dp
+                )
+            } else if (!state.isRefreshing.state.isFinished) {
+                Image(
+                    painter = painterResource(id = R.drawable.ic_arrow_left),
+                    contentDescription = "", modifier = Modifier
+                        .padding(end = 12.dp)
+                        .size(24.dp)
+                        .padding(2.dp)
+                        .graphicsLayer {
+                            this.rotationZ = rotation
+                        },
+                    colorFilter = ColorFilter.tint(color = Color(0xFF808080))
+                )
+            }
+            var headerText by remember {
+                mutableStateOf("")
+            }
+            val text = when (state.isRefreshing.state) {
+                State.IDLE -> stringResource(id = R.string.header_idle)
+                State.ReadyForAction -> stringResource(id = R.string.header_pulling)
+                State.InProgress -> stringResource(id = R.string.header_refreshing)
+                State.Success -> stringResource(id = R.string.header_complete)
+                State.Failed -> stringResource(id = R.string.header_failed)
+                else -> ""
+            }
+            if (headerText != text && text.isNotEmpty()) {
+                headerText = text
+            }
+            Text(text = headerText, color = Color(0xFF808080))
+        }
     }
 }
