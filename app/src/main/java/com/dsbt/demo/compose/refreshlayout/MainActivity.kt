0948c6e07532203bccd7cd45149849ecc7b8fc6c
wenhaiz <wenhaiz.me@gmail.com>
2023-05-09 16:37:06 +0800

    Update demo app

--- app/src/main/java/com/dsbt/demo/compose/refreshlayout/MainActivity.kt
+++ app/src/main/java/com/dsbt/demo/compose/refreshlayout/MainActivity.kt
@@ -3,12 +3,14 @@ package com.dsbt.demo.compose.refreshlayout
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.rememberLazyListState
-import androidx.compose.material3.BottomAppBar
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -17,6 +19,7 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -47,35 +50,57 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    BottomAppBar() {
-                        
-                    }
-                    RefreshLayout(
-                        state = state,
-                        contentScrollState = lazyListState,
-                        onRefresh = {
-                            refresh(scope, state)
-                        },
-                        onLoadMore = {
-                            loadMore(scope, state)
-                        },
-                        enableLoadMore = true,
-                        enableRefresh = true,
-                        header = {
-                            DefaultRefreshHeader(state = it)
-                        },
-                        footer = {
-                            DefaultRefreshFooter(state = it)
-                        }
-                    ) {
-                        LazyColumn(state = lazyListState, modifier = Modifier.fillMaxSize()) {
-                            items(data, key = { it }) {
-                                Text(
-                                    text = "text $it",
-                                    modifier = Modifier.padding(vertical = 12.dp)
-                                )
+                    Box {
+                        RefreshLayout(
+                            state = state,
+                            contentScrollState = lazyListState,
+                            onRefresh = {
+                                refresh(scope, state)
+                            },
+                            onLoadMore = {
+                                loadMore(scope, state)
+                            },
+                            enableLoadMore = true,
+                            enableRefresh = true,
+                            header = {
+                                DefaultRefreshHeader(state = it)
+                            },
+                            footer = {
+                                DefaultRefreshFooter(state = it)
+                            }
+                        ) {
+                            LazyColumn(state = lazyListState, modifier = Modifier.fillMaxSize()) {
+                                items(data, key = { it }) {
+                                    Text(
+                                        text = "text $it",
+                                        modifier = Modifier.padding(vertical = 12.dp)
+                                    )
+                                }
                             }
                         }
+
+                        Column(
+                            modifier = Modifier
+                                .align(Alignment.BottomEnd)
+                                .padding(end = 24.dp),
+                            horizontalAlignment = Alignment.CenterHorizontally
+                        ) {
+                            Button(onClick = {
+                                scope.launch {
+                                    state.refresh()
+                                }
+                            }, modifier = Modifier) {
+                                Text(text = "Refresh")
+                            }
+                            Button(onClick = {
+                                scope.launch {
+                                    state.loadMore()
+                                }
+                            }, modifier = Modifier) {
+                                Text(text = "LoadMore")
+                            }
+                        }
+
                     }
                 }
             }
